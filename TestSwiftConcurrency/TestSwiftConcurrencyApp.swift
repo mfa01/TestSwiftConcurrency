ac68cc10c735bff9bdf55dac2f52b7e88e8c1ef2
Malabed <mfa01@yahoo.com>
2024-11-24 22:15:37 +0300

    Initial Commit

---
+++ TestSwiftConcurrency/TestSwiftConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftConcurrencyApp.swift
+//  TestSwiftConcurrency
+//
+//  Created by Mohammad Alabed on 24/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
